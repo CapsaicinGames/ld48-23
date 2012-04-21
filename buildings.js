02ebbd7886003229b7890224bead491b6e12d686
andrew fray <andrew.fray@gmail.com>
2012-04-22 00:08:49 +0100

    made solar panels building use the new solar panel sprite

--- buildings.js
+++ buildings.js
@@ -53,7 +53,7 @@ function buildings_setup() {
                 newCtorCost(resourcetypes.plastic, 2),
             ],
             factory: function() {
-                return Crafty.e("Building, grass")
+                return Crafty.e("Building, solarpanelsprite")
                     .resourceDelta(resourcetypes.energy, 3);
             },
         },
