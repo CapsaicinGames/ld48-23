4276149d00b5ce4aca48cd5bb76a259f7c74d59a
Widget <widget@stdin.co.uk>
2012-04-21 12:56:59 +0100

    Abstracting out tilesize a bit

--- main.js
+++ main.js
@@ -5,17 +5,19 @@ window.onload = function() {
         .color('rgb(250,250,250)')
         .text("Click to play...");
 
-    Crafty.sprite(128, "image/sprite.png", {
+    var tilesize = 32;
+    Crafty.sprite(tilesize, "image/sprite-32.png", {
             grass: [0,0,1,1],
             stone: [1,0,1,1]
     });
-    	iso = Crafty.isometric.size(128);
+    var tileMap = new Crafty.polygon([tilesize/2,0],[tilesize,tilesize/4],[tilesize,3*tilesize/4],[tilesize/2,tilesize],[0,3*tilesize/4],[0,tilesize/4]);
+    	iso = Crafty.isometric.size(tilesize);
 	var z = 0;
-	for(var i = 20; i >= 0; i--) {
-		for(var y = 0; y < 20; y++) {
+	for(var i = asteroid.width; i >= 0; i--) {
+		for(var y = 0; y < asteroid.height; y++) {
 			var which = 1;//Crafty.randRange(0,1);
 			var tile = Crafty.e("2D, DOM, "+ (!which ? "grass" : "stone") +", Mouse")
-			.attr('z',i+1 * y+1).areaMap([64,0],[128,32],[128,96],[64,128],[0,96],[0,32]).bind("click", function(e) {
+			.attr('z',i+1 * y+1).areaMap(tileMap).bind("click", function(e) {
 				//destroy on right click
 				if(e.button === 2) this.destroy();
 			}).bind("mouseover", function() {
