32bce2ae3d4dc222e9a9d52f2c1bcd7943f426b4
andrew fray <andrew.fray@gmail.com>
2012-04-21 20:36:18 +0100

    added initial values

--- resourcetypes.js
+++ resourcetypes.js
@@ -1,14 +1,14 @@
 var resourcetypes = Object.freeze({
 
-    energy: { name: "Energy" },
-    colonists: { name: "Colonists" },
-    food: { name: "Food" },
-    ice: { name: "Ice" },
-    regolith: { name: "Regolith" },
-    steelore: { name: "Steel ore" },
-    plastic: { name: "Plastic" },
-    steel: { name: "Steel" },
-    preciousore: { name: "Precious metal ore"},
+    energy: { name: "Energy", initialValue: 100 },
+    colonists: { name: "Colonists", initialValue: 5 },
+    food: { name: "Food", initialValue: 5 },
+    ice: { name: "Ice", initialValue: 0 },
+    regolith: { name: "Regolith", initialValue: 0 },
+    steelore: { name: "Steel ore", initialValue: 0 },
+    plastic: { name: "Plastic", initialValue: 0 },
+    steel: { name: "Steel", initialValue: 0 },
+    preciousore: { name: "Precious metal ore", initialValue: 0 },
 
 });
 
