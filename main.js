6c6b3ef96a745c811d5b8dfeaa74c56af3c9ce17
Widget <widget@stdin.co.uk>
2012-04-21 12:30:08 +0100

    Borrowing Crafty.js isometric test for base

--- main.js
+++ main.js
@@ -1,89 +1,38 @@
-/*!
- * 
- *   melonJS
- *   http://www.melonjs.org
- *		
- *   Step by step game creation tutorial
- *
- **/
+window.onload = function() {
+    Crafty.init();
+    Crafty.e("2D, DOM, Text, Color")
+        .attr({x:250, y:130, w: 300 })
+        .color('rgb(250,250,250)')
+        .text("Click to play...");
 
-// game resources
-var g_resources= [];
-
-var jsApp	= {	
-    /* ---
-	   
-       Initialize the jsApp
-       
-       ---			*/
-    onload: function()
-    {
-	
-	// init the video
-	if (!me.video.init('jsapp', 448, 448, false, 1.0))
-	{
-	    alert("Sorry but your browser does not support html 5 canvas.");
-            return;
+    Crafty.sprite(128, "image/sprite.png", {
+            grass: [0,0,1,1],
+            stone: [1,0,1,1]
+    });
+    	iso = Crafty.isometric.size(128);
+	var z = 0;
+	for(var i = 20; i >= 0; i--) {
+		for(var y = 0; y < 20; y++) {
+			var which = 1;//Crafty.randRange(0,1);
+			var tile = Crafty.e("2D, DOM, "+ (!which ? "grass" : "stone") +", Mouse")
+			.attr('z',i+1 * y+1).areaMap([64,0],[128,32],[128,96],[64,128],[0,96],[0,32]).bind("click", function(e) {
+				//destroy on right click
+				if(e.button === 2) this.destroy();
+			}).bind("mouseover", function() {
+				if(this.has("grass")) {
+					this.sprite(0,1,1,1);
+				} else {
+					this.sprite(1,1,1,1);
+				}
+			}).bind("mouseout", function() {
+				if(this.has("grass")) {
+					this.sprite(0,0,1,1);
+				} else {
+					this.sprite(1,0,1,1);
+				}
+			});
+			
+			iso.place(i,y,0, tile);
+		}
 	}
-	
-	// initialize the "audio"
-	me.audio.init("mp3,ogg");
-	
-	// set all resources to be loaded
-	me.loader.onload = this.loaded.bind(this);
-	
-	// set all resources to be loaded
-	me.loader.preload(g_resources);
-        
-	// load everything & display a loading screen
-	me.state.change(me.state.LOADING);
-        
-        me.debug.renderHitBox = true;
-    },
-    
-    
-    /* ---
-       
-       callback when everything is loaded
-       
-       ---										*/
-    loaded: function ()
-    {
-	// set the "Play/Ingame" Screen Object
-	me.state.set(me.state.PLAY, new PlayScreen());
-        
-            
-        // start the game 
-	me.state.change(me.state.PLAY);
-    }
-
-}; // jsApp
-
-/* the in game stuff*/
-var PlayScreen = me.ScreenObject.extend(
-{
-
-    onResetEvent: function()
-    {	
-    
-    },
-    
-	
-    /* ---
-       
-       action to perform when game is finished (state change)
-       
-       ---	*/
-    onDestroyEvent: function()
-    {
-	
-    }
-    
-});
-
-
-//bootstrap :)
-window.onReady(function() 
-{
-	jsApp.onload();
-});
+}
