0a2f9cbf2cbafdcfa354ed41897fc68e73f20b4f
andrew fray <andrew.fray@gmail.com>
2012-04-21 22:08:25 +0100

    better resource delta construction, and mines will complete their resource deltas on being built.

--- buildings.js
+++ buildings.js
@@ -5,26 +5,38 @@ var buildingBlueprints = {};
 function initBuildings() {
     Crafty.c("Building", {
         _colonists: 1,
-        Building: function(resourceDeltas) {
-            this._resourceDeltas = resourceDeltas;
+        _resourceDeltas: {},
+        
+        init: function() {
+            this._resourceDeltas = {};
+        },
+
+        resourceDelta: function(resource, delta) {
+            this._resourceDeltas[resource.name] = delta;
             return this;
         },
         isActive: function() {
             return this._colonists > 0; 
         },
+        onBuild: function(tileResource) {
+            // intentionally blank
+        },
     });
 
     buildingBlueprints = {
-        "Ice Mine": Crafty.e("Building")
-            .Building(new ResourceDeltas()
-                      .add(resourcetypes.energy, -1)
-                      .add(resourcetypes.ice, 1)
-                     ),
+        "Mine": createMine(-1, 1),
+        "Super Mine": createMine(-2, 2),
         "Solar Panel": Crafty.e("Building")
-            .Building(new ResourceDeltas()
-                      .add(resourcetypes.energy, 2)
-                     ),
+            .resourceDelta(resourcetypes.energy, 3),
     };
 
+}
+
+function createMine(powerDrain, resourceProduction) {
+    return Crafty.e("Building")
+        .resourceDelta(resourcetypes.energy, powerDrain)
+        .attr("onBuild", function(tileResource) {
+            this.resourceDelta(tileResource, resourceProduction);
+        });
 }
 
