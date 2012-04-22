fab3c769d6d146592ab5a5dea436b09302d0502e
andrew fray <andrew.fray@gmail.com>
2012-04-22 22:11:16 +0100

    switch graphic depening on which input we're missing

--- buildings.js
+++ buildings.js
@@ -48,22 +48,26 @@ function buildings_setup() {
         onTick: function() {
             // intentionally blank
         },
-        showOverlay: function(overType) {
+        showOverlay: function(overType, param) {
             if (overType !== this.overlayType) {
                 switch (overType) {
-                    case "no":
-                        this.overlay.visible = false;
-                        break;
-
-                    case "res":
-                        this.overlay.visible = true;
+                case "no":
+                    this.overlay.visible = false;
+                    break;
+                    
+                case "res":
+                    this.overlay.visible = true;
+                    if (param.indexOf(resourcetypes.energy.name) >= 0) {
+                        this.overlay.sprite(0,1);
+                    } else {
                         this.overlay.sprite(1,0);
-                        break;
-
-                    case "inactive":
-                        this.overlay.visible = true;
-                        this.overlay.sprite(0,0);
-                        break;
+                    }
+                    break;
+                    
+                case "inactive":
+                    this.overlay.visible = true;
+                    this.overlay.sprite(0,0);
+                    break;
                 }
                 this.overlayType = overType;
             }
@@ -266,10 +270,10 @@ function buildings_setup() {
 }
 
 function build(blueprint, tileToBuildOn) {
-    var over = Crafty.e("BuildingInfoOverlay, regolithOverlay")
+    var over = Crafty.e("BuildingInfoOverlay, outOfPowerOverlay")
         .attr({x: tileToBuildOn.x,
                y: tileToBuildOn.y,
-               z: 999});
+               z: 999999});
     var bldg = blueprint.factory()
         .attr({x: tileToBuildOn.x,
                y: tileToBuildOn.y - tilesize/2,
